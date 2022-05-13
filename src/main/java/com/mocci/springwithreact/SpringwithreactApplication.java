58d43e5d010d7704a9b5fb487d7599cb7ba143cf
mocci <tjdwhd3345@gmail.com>
2022-05-13 17:06:17 +0900

    git init

---
+++ src/main/java/com/mocci/springwithreact/SpringwithreactApplication.java
@@ -0,0 +1,13 @@
+package com.mocci.springwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithreactApplication.class, args);
+	}
+
+}
